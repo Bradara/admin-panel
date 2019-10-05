643a85c14054bd02351f00b6f41e6a33b5f95482
Penko Georgiev <penko.georgiev@gmail.com>
2019-10-05 16:16:42 +0300

    Add Routes - channes - add and View

--- server/controllers/home-controller.js
+++ server/controllers/home-controller.js
@@ -4,5 +4,12 @@ module.exports = {
   },
   about: (req, res) => {
     res.render('home/about')
+  },
+  channelsView:(req,res) => {
+    res.render('/channels/view')
+  },
+  channelsAdd:(req,res) =>{
+    res.render('/channels/add')
   }
+
 }
