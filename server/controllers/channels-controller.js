fb7917f6b7607dd47bb7faa46c4f33d9320d1a33
Bradara <atanas.nikolov74@gmail.com>
2019-10-17 23:17:01 +0300

    Update 171019

--- server/controllers/channels-controller.js
+++ server/controllers/channels-controller.js
@@ -2,9 +2,13 @@ const Channel = require('../data/Channel')
 
 module.exports = {
     get: (req, res) => {
-        let channels = Channel.findAll()
-        res.locals.channels = channels
-        res.render('channels/view')
+        let channels = Channel.findAll().then(c => {
+            res.locals.channels = c
+            res.render('channels/view')
+        }).catch(err => {
+            res.locals.globalError = err
+            res.render('channels/view')
+        })
     },
     add: (req, res) => {
         res.render('channels/add')
@@ -24,9 +28,12 @@ module.exports = {
             map_pmt: channel.map_pmt,
             map_video: channel.map_video,
             map_audio: channel.map_audio,
-            enable: channel.enable
+            enable: channel.enable == 'on'
         }).then(()=>{
             res.redirect('/channels/view')
+        }).catch(err => {
+            res.locals.globalError = err
+            res.render('channels/add')
         })
     }
 }
